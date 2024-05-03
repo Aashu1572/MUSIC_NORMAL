8846211111d573105d02577868239aa22fd48e4a
SPY_X_Family 🇮🇳 <161732428+Aashu1572@users.noreply.github.com>
2024-05-03 11:25:22 +0530

    Update userbot.py

--- AnieXEricaMusic/core/userbot.py
+++ AnieXEricaMusic/core/userbot.py
@@ -51,12 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("AmBotYT")
-                await self.one.join_chat("AM_YTSupport")
-                await self.one.join_chat("AbhiModszYT_Return")
-                await self.one.join_chat("TGGojoSatoRu")
-                await self.one.join_chat("SuperBanSBots")
-                await self.one.join_chat("About_AMBot")
+                await self.one.join_chat("The_Friend_Zone")
+                await self.one.join_chat("Khan_Sir_Free_Batch_Upsc")
             except:
                 pass
             assistants.append(1)
@@ -76,12 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("AmBotYT")
-                await self.two.join_chat("AM_YTSupport")
-                await self.two.join_chat("AbhiModszYT_Return")
-                await self.two.join_chat("TGGojoSatoRu")
-                await self.two.join_chat("SuperBanSBots")
-                await self.two.join_chat("About_AMBot")
+                await self.one.join_chat("The_Friend_Zone")
+                await self.one.join_chat("Khan_Sir_Free_Batch_Upsc")
             except:
                 pass
             assistants.append(2)
@@ -101,12 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("AmBotYT")
-                await self.three.join_chat("AM_YTSupport")
-                await self.three.join_chat("AbhiModszYT_Return")
-                await self.three.join_chat("TGGojoSatoRu")
-                await self.three.join_chat("SuperBanSBots")
-                await self.three.join_chat("About_AMBot")
+                await self.one.join_chat("The_Friend_Zone")
+                await self.one.join_chat("Khan_Sir_Free_Batch_Upsc")
             except:
                 pass
             assistants.append(3)
@@ -126,12 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("AmBotYT")
-                await self.four.join_chat("AM_YTSupport")
-                await self.four.join_chat("AbhiModszYT_Return")
-                await self.four.join_chat("TGGojoSatoRu")
-                await self.four.join_chat("SuperBanSBots")
-                await self.four.join_chat("About_AMBot")
+                await self.one.join_chat("The_Friend_Zone")
+                await self.one.join_chat("Khan_Sir_Free_Batch_Upsc")
             except:
                 pass
             assistants.append(4)
@@ -151,12 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("AmBotYT")
-                await self.five.join_chat("AM_YTSupport")
-                await self.five.join_chat("AbhiModszYT_Return")
-                await self.five.join_chat("TGGojoSatoRu")
-                await self.five.join_chat("SuperBanSBots")
-                await self.five.join_chat("About_AMBot")
+                await self.one.join_chat("The_Friend_Zone")
+                await self.one.join_chat("Khan_Sir_Free_Batch_Upsc")
             except:
                 pass
             assistants.append(5)
